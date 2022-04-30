a714759c2b3f81e0c05838e45c853d7997565b68
tmtcam-042 <90966515+tmtcam-042@users.noreply.github.com>
2022-04-30 19:27:02 +1000

    Fixed typo in criterion string on line 30 of test_battle.py

--- test_battle.py
+++ test_battle.py
@@ -27,7 +27,7 @@ class TestBattle(TesterBase):
             return
         try:
             with captured_output("2 2 1\n 0 2 1") as (inp, out, err):
-                result = b.optimised_mode_battle("hp", "level")
+                result = b.optimised_mode_battle("hp", "lvl")
         except Exception as e:
             self.verificationErrors.append(f"Battle failed to execute: {str(e)}.")
             return
