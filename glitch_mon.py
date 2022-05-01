4b66dabe6de17036d10f47bc6acca0be695a44bb
Neth <nbot0003@student.monash.edu>
2022-05-01 18:04:07 +1000

    commenting

--- glitch_mon.py
+++ glitch_mon.py
@@ -3,31 +3,44 @@ from abc import abstractmethod
 
 from pokemon_base import PokemonBase
 
+""" Base class for Glitch Mon
+
+Defines an abstract class that inherits another generic abstract class, PokemonBase
+to create a glitch mon.
+"""
+__author__ = "Neth Botheju"
+__docformat__ = 'reStructuredText'
 
 class GlitchMon(PokemonBase):
+    """ Abstract class for Missing No Pokémon """
     def __init__(self, hp: int, poke_type: str):
+        """ initalises the arguments from PokemonBase """
         PokemonBase.__init__(self, hp, poke_type)
 
     @abstractmethod
     def increaseHp(self) -> int:
+        """ abstract method that increases the HP integer
+        of the pokemon """
         pass
 
     def superPower(self) -> str:
+        """ uses a random integer generator (25% chance) to
+        determine if Glitchmon should level up, increase HP or both
+
+        return: the string that indicates which powerup MissingNo has used
+        """
         num = random.randint(0, 2)
         if num == 0:
             self.level_up()
             output = self.get_name() + " leveled up!"
             return output
-            print(output)
         elif num == 1:
             self.increaseHp()
             output = self.get_name() + " increased HP!"
             return output
-            print(output)
 
         else:
             self.level_up()
             self.increaseHp()
             output = self.get_name() + " leveled up and increased HP!"
             return output
-            print(output)
